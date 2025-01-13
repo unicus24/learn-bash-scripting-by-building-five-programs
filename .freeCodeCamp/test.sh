78f40717feb30fbc8e29a06e511f0e5ca0e2a2eb
Mark Anthony <senormarkanthonykamau@gmail.com>
2025-01-13 09:24:37 +0000

    Save progress: 1220.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -8,5 +8,7 @@ echo ${RESPONSES[$N]}
 N=$(( RANDOM % 6 ))
 GET_FORTUNE() {
   echo Ask a yes or no question:
+  read QUESTION
 }
 GET_FORTUNE
+echo $QUESTION
--- fortune.sh
+++ fortune.sh
@@ -8,5 +8,6 @@ echo ${RESPONSES[$N]}
 N=$(( RANDOM % 6 ))
 GET_FORTUNE() {
   echo Ask a yes or no question:
+  read QUESTION
 }
 GET_FORTUNE
