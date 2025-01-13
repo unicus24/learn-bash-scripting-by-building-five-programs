fd7de440ceb357092ce2dc627d1119acd54456bd
Mark Anthony <senormarkanthonykamau@gmail.com>
2025-01-13 09:16:22 +0000

    Save progress: 1180.1

--- fortune.sh
+++ fortune.sh
@@ -6,3 +6,5 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 echo ${RESPONSES[$N]}
 N=$(( RANDOM % 6 ))
+GET_FORTUNE() {
+}
